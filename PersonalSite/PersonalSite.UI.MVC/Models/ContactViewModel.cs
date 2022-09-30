506a061870111663c532c12668d5702d76c0a301
SeanCodes2 <sean.coyan@outlook.com>
2022-09-30 14:57:50 -0500

    MovedEverythingOver

---
+++ PersonalSite/PersonalSite.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSite.UI.MVC.Models
+{
+    [Keyless]
+    public class ContactViewModel
+    {        
+        [Required(ErrorMessage = "*Name Is Required")] 
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "*Email Is Required")]
+        [DataType(DataType.EmailAddress)] 
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "*Subject Is Required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "*Message Is Required")]
+        [DataType(DataType.MultilineText)] 
+        public string Message { get; set; }
+    }
+}
