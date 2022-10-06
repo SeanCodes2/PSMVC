ebc8a8641a0b81677facf76307dd1ead22576d13
SeanCodes2 <sean.coyan@outlook.com>
2022-10-06 07:30:21 -0500

    FinishedAPI

--- PersonalSite/PersonalSite.UI.MVC/wwwroot/js/scripts.js
+++ PersonalSite/PersonalSite.UI.MVC/wwwroot/js/scripts.js
@@ -1,11 +1,33 @@
-/*!
-* Start Bootstrap - Modern Business v5.0.6 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
 
 
 
+//var btn = $('#button');
+//$(window).scroll(function () {
+//    if ($(window).scrollTop() > 300) {
+//        btn.addClass('show');
+//    } else {
+//        btn.removeClass('show');
+//    }
+//});
+
+//btn.on('click', function (e) {
+//    e.preventDefault();
+//    $('html, body').animate({ scrollTop: 0 }, '300');
+//});
+
+
+//$(Function() {
+//    $.ajax({
+//        url: 'https://api.chucknorris.io/jokes/random?category=history',
+//        method: 'GET',
+//        success: function (data) {
+//            console.table(data.results)
+//            var facts = data.results
+//            var factsResults = document.getElementById('factsResults')
+//            facts.map(x => factsResults.innerHTML +=
+//                '<div class="col-lg-4 col-md-6 col-sm-12 p-3">< div class= "fact" ><h3>${x.categories}</h3></div ></div >'
+//            )
+//        }
+//    })
+//})
 
